7456479cdf8672cd50a5953bf03841d309897597
sy-yingjin <shaira.sy@student.ateneo.edu>
2025-06-19 12:39:48 +0800

    finalized qc2

--- db-tools/scripts/qc2_obs_valuecheck.py
+++ db-tools/scripts/qc2_obs_valuecheck.py
@@ -24,8 +24,9 @@ def get_minmax(file, var):
     config_df = pd.read_csv(config_file, usecols=[
         'var','min','max'
     ])
-    max = config_df.loc[(config_df['var'] == var), 'max'].iloc[0]
-    min = config_df.loc[(config_df['var'] == var), 'min'].iloc[0]
+    max = config_df.loc[(config_df['var'] == var),'max'].item()
+    min = config_df.loc[(config_df['var'] == var),'min'].item()
+
     return [min, max]
 
 if __name__ == "__main__": 
@@ -51,70 +52,94 @@ if __name__ == "__main__":
     # create new columns for qc2
     out_df['timestamp'] = ''
     out_df['id'] = ''
-    out_df['qc2-flagged_error'] = ''
+    out_df['flagged_error'] = ''
     out_df['qc2-flagged_var'] = ''
     out_df['qc2-flagged_data'] = ''
 
-    # indicate the date of log retrieval if there are files to log
-    if files:
-        log = main_dir / f"{yyyy}/{mm}/obs_logs/{file_prefix}-{yyyy}{mm}-qc2-log.txt"
-        log.parent.mkdir(parents=True, exist_ok=True)
+    # loop through the files
+    for file in files:
+        df = pd.read_csv(file, index_col=0)
+        stn_id = re.findall(f"{yyyy}{mm}-([\\d]+).csv", os.path.basename(file))  # noqa: E501
 
-    # open file for logging while checks run
-    with open(log, 'w') as f:
-        f.write("===LEVEL 2: RANGE VALUE CHECKS===\nDatapoints outside valid range:\n")
+        print(f"Checking validity of data from station id {stn_id[0]}...")
 
-        # loop through the files
-        for file in files:
-            df = pd.read_csv(file, index_col=0)
-            stn_id = re.findall(f"{yyyy}{mm}-([\\d]+).csv", os.path.basename(file))  # noqa: E501
+        '''=========================================
+        VALIDITY RANGE CHECKS: Checks for if the data falls within the valid data range
+        will raise the `invalid range` flag
 
-            print(f"Checking validity of data from station id {stn_id[0]}...")
+        '''
+        test_range = ['temp', 'srad', 'pres', 'rh', 'wdir', 'wspd']
+        for variable in test_range:
+            minmax = get_minmax(file, variable)
 
-            f.write(f"\n\nOBSERVATIONS FLAGGED FROM STATION ID# {stn_id[0]}\n")
+            for index, value in df.loc[
+                (df[variable] < minmax[0]) | (df[variable] > minmax[1]), variable
+            ].items():
+                obs_id = df['id'].get(index)
 
-            '''
-            VALIDITY RANGE CHECKS: Checks for if the data falls within the valid data range
-            will raise the `invalid range` flag
+                # store data into the dataframe
+                out_df.loc[-1] = [2, stn_id[0],'','','',index,obs_id,'invalid range', variable, value]
+                out_df.index += 1
+                out_df.sort_index
 
-            '''
-            test_range = ['temp', 'srad', 'pres', 'rh', 'wdir', 'wspd']
-            for variable in test_range:
-                f.write(f"\nTimestamps where {variable} is outside the accepted range:\n")
-                minmax = get_minmax(file, variable)
+        # Temperature
+        # minmax = get_minmax(file,'temp')
+        # # test_range = ['temp', 'srad', 'pres', 'rh', 'wdir', 'wspd']
+        # for index, value in df.loc[
+        #     (df['temp'] < 15) | (df['temp'] > 40), 'temp'
+        # ].items():
+        #     obs_id = df['id'].get(index)
+        #     print(f" - observed {value} at {stn_id[0]} at time {index}")
 
-                for index, value in df.loc[
-                    (df[variable] < minmax[0]) | (df[variable] > minmax[1]), variable
-                ].items():
-                    obs_id = df['id'].get(index)
-                    f.write(" - Flagged: `Invalid Range`\n")
-                    f.write(f" -- observed {value}\n")
-                    f.write(f" -- Station={stn_id[0]}, timestamp={index}\n")
-                    f.write(f" -- Obs_id={obs_id},\n")
+        #     # store data into the dataframe
+        #     out_df.loc[-1] = [2, stn_id[0],'','','',index,obs_id,'invalid range', 'temp', value]
+        #     out_df.index += 1
+        #     out_df.sort_index
 
-                    # store data into the dataframe
-                    out_df.loc[-1] = [2, stn_id[0],'','','',index,obs_id,'invalid range', variable, value]
-                    out_df.index += 1
-                    out_df.sort_index
 
-            '''
-            COHESIVE LOGIC CHECKS: Checks for contradictions or logic between variables
-            will raise the `incohesive_data` flag
-            '''
+        '''=========================================
+        COHESIVE LOGIC CHECKS: Checks for contradictions or logic between variables
+        will raise the `incohesive data` flag
+        '''
+        # Solar Radiation detected at night:
+        for index, value in df.loc[
+            (df['srad'] > 0) & ((df['hour'] > 18) | (df['hour'] < 5)), 'srad'
+        ].items():
+            obs_id = df['id'].get(index)
+
+            # store data into the dataframe
+            out_df.loc[-1] = [2, stn_id[0],'','','',index,obs_id,'incohesive data', 'srad', value]
+            out_df.index += 1
+            out_df.sort_index
             
-            '''
-            CHANGE RATE CHECKS: Checks the temporal difference or sum of variables
-            will raise the `invalid_rate` flag
-            '''
-            # TO DO: investigate the rainfall rate in `pgAdmin4` to see what the range means from the slides
+        # Temperature (temp) - Dewpoint (td) should be greater than 0.2
+        for index, value in df.loc[
+            ((df['temp'] - df['td']) <= 0.2), 'td'
+        ].items():
+            obs_id = df['id'].get(index)
 
-            # output a csv
-            out_file = main_dir / f"{yyyy}/{mm}/obs_logs/{file_prefix}-{yyyy}{mm}-log.csv"
-            out_file.parent.mkdir(parents=True, exist_ok=True)
-            out_df.to_csv(out_file, index=False)
-
-            # mark datapoints as within valid range
-            df['qc_level'] = df['qc_level'].mask(df['qc_level'] == 1, other=2)
-            df.to_csv(file)
-
+            # store data into the dataframe
+            out_df.loc[-1] = [2, stn_id[0],'','','',index,obs_id,'incohesive data', 'td', value]
+            out_df.index += 1
+            out_df.sort_index
             
+        # Wind Direction exists when Wind Speed is 0
+        for index, value in df.loc[
+            (df['wdir'].notna()) & (df['wspd'] == 0), 'wdir'
+        ].items():
+            obs_id = df['id'].get(index)
+
+            # store data into the dataframe
+            out_df.loc[-1] = [2, stn_id[0],'','','',index,obs_id,'incohesive data', 'wdir', value]
+            out_df.index += 1
+            out_df.sort_index
+            
+
+        # output a csv
+        out_file = main_dir / f"{yyyy}/{mm}/obs_logs/{file_prefix}-{yyyy}{mm}-log.csv"
+        out_file.parent.mkdir(parents=True, exist_ok=True)
+        out_df.to_csv(out_file, index=False)
+
+        # mark datapoints as within valid range
+        df['qc_level'] = df['qc_level'].mask(df['qc_level'] == 1, other=2)
+        df.to_csv(file)
